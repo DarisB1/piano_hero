4a4795e1f984a5a1414331d5eda723ee9307acad
DarisB1 <dboumertit@edenschool.fr>
2024-04-10 11:26:50 +0200

    piano-first

---
+++ main.js
@@ -0,0 +1,60 @@
+const piano = document.querySelector('.div-total');
+
+for (let i = 0; i < 2; i++) {
+  let toucheBlanche = document.createElement('div')
+  toucheBlanche.classList.add("blanc");
+  piano.appendChild(toucheBlanche);
+  let toucheNoir = document.createElement('div')
+  toucheNoir.classList.add("noir");
+  toucheBlanche.appendChild(toucheNoir);
+  // toucheBlanche.style.backgroundColor = 'blue';
+}
+
+let toucheBlanche2 = document.createElement('div')
+  toucheBlanche2.classList.add("blanc");
+  piano.appendChild(toucheBlanche2);
+
+  for (let i = 0; i < 3; i++) {
+    let toucheBlanche = document.createElement('div')
+    toucheBlanche.classList.add("blanc");
+    piano.appendChild(toucheBlanche);
+    let toucheNoir = document.createElement('div')
+    toucheNoir.classList.add("noir");
+    toucheBlanche.appendChild(toucheNoir);
+    // toucheBlanche.style.backgroundColor = 'blue';
+  }
+
+  toucheBlanche2 = document.createElement('div')
+  toucheBlanche2.classList.add("blanc");
+  piano.appendChild(toucheBlanche2);
+
+  for (let i = 0; i < 2; i++) {
+    let toucheBlanche = document.createElement('div')
+    toucheBlanche.classList.add("blanc");
+    piano.appendChild(toucheBlanche);
+    let toucheNoir = document.createElement('div')
+    toucheNoir.classList.add("noir");
+    toucheBlanche.appendChild(toucheNoir);
+    // toucheBlanche.style.backgroundColor = 'blue';
+  }
+
+  toucheBlanche2 = document.createElement('div')
+  toucheBlanche2.classList.add("blanc");
+  piano.appendChild(toucheBlanche2);
+
+  for (let i = 0; i < 3; i++) {
+    let toucheBlanche = document.createElement('div')
+    toucheBlanche.classList.add("blanc");
+    piano.appendChild(toucheBlanche);
+    let toucheNoir = document.createElement('div')
+    toucheNoir.classList.add("noir");
+    toucheBlanche.appendChild(toucheNoir);
+    // toucheBlanche.style.backgroundColor = 'blue';
+  }
+
+  toucheBlanche2 = document.createElement('div')
+  toucheBlanche2.classList.add("blanc");
+  piano.appendChild(toucheBlanche2);
+ 
+
+  
